b9650152d63d70abc69cedef549e7ff19c593b01
Vignesh193209 <146579569+Vignesh193209@users.noreply.github.com>
2025-12-13 17:28:13 +0530

    Update java1.java
    add delete method

--- java1.java
+++ java1.java
@@ -1,10 +1,14 @@
 class crud
 {
-    int add[]=new int[5];
-    performadd 
+    int dlt[]=new int[5];
+   
+    //performadd 
     //perform delete operation
 }
 public static void performadd(int add){
     
+    
+}
+public static void perfromdlt(int dlt) {
     
 }
