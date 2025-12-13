a47eaeacb5dab139b929c7ff85d94e169b112a5f
S.Ramadoss <145978900+Ramadoss2003@users.noreply.github.com>
2025-12-13 17:04:49 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,20 +1,6 @@
-import java.util.*;
-
-public class LeapYear {
-
-	static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {
-		
-		System.out.print("Enter your year :");
-		int year = sc.nextInt();
-		Leapyear(year);
-	}
-	public static void Leapyear(int year){
-
-		if (year % 4 == 0) {
-			System.out.println("Leap year");
-		}  else {
-			System.out.println("Not a leap year");
-		}
-	}
+class crud
+{
+    int a[]=new int[5];
+    //perform add 
+    //perform delete operation
 }
